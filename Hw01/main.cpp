6b6da6f9090f349a2c4f64384eeb7ecb8f059a6f
Alexander Bedrossian <admin@172-16-24-229.DYNAPOOL.NYU.EDU>
2015-02-05 17:19:15 -0500

    Initial Commit

---
+++ Hw01/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hw01
+//
+//  Created by Alexander Bedrossian on 2/5/15.
+//  Copyright (c) 2015 Alexander Bedrossian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
